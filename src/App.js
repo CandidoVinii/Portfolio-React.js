a9ba0aa55f5a429dc6661f63603029fad1f02444
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-04-24 15:27:28 -0300

    Fix: the bugssss

--- src/App.js
+++ src/App.js
@@ -6,20 +6,17 @@ import Main from './pages/Main/Main';
 import Sobre from './pages/Sobre/Sobre';
 import Projetos from './pages/Projetos/Projetos';
 import React from 'react';
-import { Router } from 'react-router-dom';
-import history from './Components/history';
+
 
 class App extends React.Component {
   render() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Router history={history}>
           <Switch>
             <Route exact path="/" component={Main} />
             <Route exact path="/sobre" component={Sobre} />
             <Route exact path="/projetos" component={Projetos} />
           </Switch>
-        </Router>
     </BrowserRouter>
     );
   }
